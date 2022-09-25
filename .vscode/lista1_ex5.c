62d14bafb6b7246d7aadbc522169704604f9dc00
leoribeiro61 <leonardo4600@gmail.com>
2022-09-25 18:28:44 -0300

    lista1_ex5.c

---
+++ .vscode/lista1_ex5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int quantidadeRolos , quantidadeMetros , metros , rolos = 50;
+
+    printf("Digite a quantidade necessaria de metros que ira precisar: \n");
+    scanf("%d", &metros);
+
+    quantidadeRolos = (metros / rolos);
+    quantidadeMetros = (metros % 50); 
+
+    printf("Quantidade necessaria de rolos: %d rolos \n", quantidadeRolos);
+
+    printf("Quantidade de metros avulsos: %d", quantidadeMetros);
+
+}
