92b14d8693f76af66dd98ab80f969d89dfb671cb
leoribeiro61 <leonardo4600@gmail.com>
2022-11-30 22:32:46 -0300

    Exercicios_String

---
+++ lista5/lista1_ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+	char cor[20];
+
+    printf("Digite uma cor: \n");
+    gets(cor);
+
+    if ((strcmp (cor,"branco") == 0) || (strcmp(cor,"verde") == 0) || (strcmp(cor,"amarelo") == 0))
+        printf("Cor valida!");
+        else
+            printf("cor invalida");
+}
